29356bc3c69af5bed210c7b368171e7057b004e9
Karl Anthony Baluyot <sovietkamarov@gmail.com>
2019-06-28 13:22:38 +0800

    added benchmark test

--- gojson_test.go
+++ gojson_test.go
@@ -14,3 +14,13 @@ func TestOrderedMap(t *testing.T) {
 	res := om.ToJSON("age")
 	fmt.Println(res)
 }
+
+func BenchmarkOrderedMap(t *testing.B) {
+	om := OrderedMap{
+		"name": "karl",
+		"age":  21,
+	}
+
+	res := om.ToJSON("age")
+	fmt.Println(res)
+}
